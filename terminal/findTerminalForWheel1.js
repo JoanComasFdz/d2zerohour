009f03b602ea1bfa71c05d515e4dfd6cd90568ba
Joan Comas Fernandez <joan.comas.fdz@gmail.com>
2019-05-14 20:30:44 +0200

    finished findTerminalForWheel1 working on consoles12

--- terminal/findTerminalForWheel1.js
+++ terminal/findTerminalForWheel1.js
@@ -17,129 +17,74 @@ function findTerminalForWheel1(data, firstConsoleLeftValue, useLeftValueForThird
   const positionWhenSpliting = useLeftValueForThirdWheel ? 0 :  1
   const wheelNumber = useLeftValueForThirdWheel ? 3 : 4
 
+  console.info(`Finding terminal for wheel 1 with third wheel numner '${wheelNumber}' and splitting position '${positionWhenSpliting}'...`)
+
   clearSecondWheel()
   clearThirdWheel(wheelNumber)
 
-  const matchingRightValues = getFirstConsoleRightValuesMatchingTheLeftValue(data, firstConsoleLeftValue)
+  const matchingRightValuesForFirstConsoleLeftValue = getFirstConsoleRightValuesMatchingTheLeftValue(data, firstConsoleLeftValue)
 
-  activateValuesInSecondWheel(matchingRightValues)
+  activateValuesInSecondWheel(matchingRightValuesForFirstConsoleLeftValue)
 
-  if (matchingRightValues.length === 0) {
+  if (matchingRightValuesForFirstConsoleLeftValue.length === 0) {
     // There is no matching value on the second wheel.
     // Nothing else to do.
     console.info(`Finished finding terminal for wheel 1 because the are no matching values on the second wheel.`)
     return
   }
 
-  if (matchingRightValues.length > 1) {
+  if (matchingRightValuesForFirstConsoleLeftValue.length > 1) {
     // There is more than 1 matching value on the second wheel.
     // It needs to be clicked manually.
     // Nothing else to do.
-    console.info(`Finished finding terminal for wheel 1 because the are '${matchingRightValues.length}' values matching on the second wheel. `)
+    console.info(`Finished finding terminal for wheel 1 because the are '${matchingRightValuesForFirstConsoleLeftValue.length}' values matching on the second wheel. `)
     return
   }
 
-  firstConsolePair = matchingRightValues[0]
+  firstConsoleRightValue = matchingRightValuesForFirstConsoleLeftValue[0]
+  firstConsolePair = firstConsoleLeftValue + '-' + firstConsoleRightValue
   console.info(`Found a single pair matching first console left value '${firstConsoleLeftValue}': ${firstConsolePair}`)
+  
+  $('#console1PairResult span').html(firstConsolePair); 
+  $(`#wheel2 #cn-button`).html(firstConsoleRightValue);
 
-  $('#firstConsolePairResult span').html(firstConsolePair); 
+  // There is no left and right variable naming from now on,
+  // since it depends on the parameter positionWhenSpliting
 
+  const secondConsolePairs = getSecondConsolePairsForFirstConsolePair(data[firstConsolePair])
+
+  activateValuesInThirdWheel(secondConsolePairs, wheelNumber, positionWhenSpliting)
+
+  if (secondConsolePairs.length === 0) {
+    // There is no matching value on the third wheel.
+    // Nothing else to do.
+    console.info(`Finished finding terminal for wheel 1 because the are no matching values on the third wheel.`)
+    return
+  }
+
+  if (secondConsolePairs.length > 1) {
+    // There is more than 1 matching value on the third wheel.
+    // It needs to be clicked manually.
+    // Nothing else to do.
+    console.info(`Finished finding terminal for wheel 1 because the are '${secondConsolePairs.length}' values matching on the third wheel. `)
+    return
+  }
+
+  secondConsolePair = secondConsolePairs[0]
+  secondConsoleValue = secondConsolePair.split('-')[positionWhenSpliting]
+  console.info(`Found a single pair in the second console matching first console pair '${firstConsolePair}': ${secondConsolePair}`)
+  
+  $('#console2PairResult span').html(firstConsolePair); 
+  $(`#wheel${wheelNumber} #cn-button`).html(secondConsoleValue);
+
+  const terminal = data[firstConsolePair][secondConsolePair]
+  console.info (`Terminal is ${terminal}`)
+  $('#nodeToActivateValue').html(terminal);
+  color = terminal.split(' ')[0];
+  var nodeToActivate = document.getElementById('nodeToActivateValue');
+  nodeToActivate.style.color = color;
 
   return
-
-
-  firstConsolePair =  firstConsoleLeftValue + '-' + potentialFirstConsoleRightValue
-  console.info(`firstConsolePair = ${firstConsolePair}`)
-  $('#firstConsolePairResult span').html(firstConsolePair);
-
-  if(firstConsolePairKeys.length == 1)
-  {
-    // Only one possibility found under firstConsolePair, so no need to search further,
-    // just use that one.
-    const key = firstConsolePairKeys[0]
-    const element = data[firstConsolePair][key]
-    console.info(`There is only one possible combo, so lets print this: ${JSON.stringify(element)}`)
-    $('#nodeToActivateValue').html(element);
-    color = element.split(' ')[0];
-    var nodeToActivate = document.getElementById('nodeToActivateValue');
-    nodeToActivate.style.color = color;
-
-    secondConsoleValueToSelect = key.split('-')[positionWhenSpliting]
-    $(`#wheel${wheelNumber} #cn-button`).html(secondConsoleValueToSelect);
-    $('#secondConsolePairResult span').html(key);
-    firstConsolePair = ''
-
-    console.info(`Console 1 Pair has only one combo and it has been automatically selected: ${JSON.stringify(element)}`)
-
-    return
-  }
-
-  console.info(`Console 1 has more that 1 combo.`)
-  // Console 1 has more than 1 combos.
-
-  secondConsolePair =  secondConsoleLeftValue + '-' + secondConsoleRightValue 
-  console.info(`secondConsolePair = ${secondConsolePair}`)
-  $('#secondConsolePairResult span').html(secondConsolePair);
-
-  thirdWheelSelectedValue = secondConsolePair.split('-')[positionWhenSpliting]
-  if (secondConsolePair.length > 1 && thirdWheelSelectedValue !== '')  {
-
-    console.info(`Wheel 3 value has been clicked, see if there is only one combo with it...`)
-
-    var elementsMatchingThirdWheelValue = 0
-    var lastElementMatchingThirdWheelValue = ''
-    for (let i = 0; i < firstConsolePairKeys.length; i++) {
-      const element = firstConsolePairKeys[i];
-      var elementPotentialMatchingValue = element.split('-')[positionWhenSpliting]
-      console.info(`Analyzing element: ${JSON.stringify(element)}, does it match with wheel 3 value ${thirdWheelSelectedValue}?`)
-      if (elementPotentialMatchingValue === thirdWheelSelectedValue  ) {
-        elementsMatchingThirdWheelValue++
-        lastElementMatchingThirdWheelValue = element
-        console.info(`Yes, element: ${JSON.stringify(element)} matches ${thirdWheelSelectedValue }.`)
-      } else {
-        console.info(`No, element: ${JSON.stringify(element)} does not match ${thirdWheelSelectedValue }.`)
-
-      }
-    }
-
-    if (elementsMatchingThirdWheelValue === 1) {
-      console.info (`There is only 1 combo matching ${thirdWheelSelectedValue }, select it automatically: ${lastElementMatchingThirdWheelValue}`)
-      secondConsolePair = lastElementMatchingThirdWheelValue
-      $('#secondConsolePairResult span').html(secondConsolePair);
-      const terminal = data[firstConsolePair][secondConsolePair]
-      console.info (`Terminal is ${terminal}`)
-      $('#nodeToActivateValue').html(terminal);
-      color = terminal.split(' ')[0];
-      var nodeToActivate = document.getElementById('nodeToActivateValue');
-      nodeToActivate.style.color = color;
-      
-      return
-    }
-  }
-
-  console.info(`Console 1 pair exists, has more than one combo, and the console 2 left value has more than one combo...`)
-
-  if (!data[firstConsolePair]) {
-    console.info('ENTERED VERY LAST IF !!!!')
-  } else {
-    console.info('ENTERED VERY LAST IF ELSE !!!!')
-    $('#nodeToActivateValue').html('Ok, select next pair.');
-    var nodeToActivate = document.getElementById('nodeToActivateValue');
-    nodeToActivate.style.color = 'green'; 
-
-    if(data[firstConsolePair][secondConsolePair]) {
-      console.info("Console 1 Console 2 pair found!")
-      $('#nodeToActivateValue').html(data[firstConsolePair][secondConsolePair]);
-      color = data[firstConsolePair][secondConsolePair].split(' ')[0];
-      var nodeToActivate = document.getElementById('nodeToActivateValue');
-      nodeToActivate.style.color = color; 
-    } else {
-      console.info(`Console 1 Console 3 pair NOT found: ${firstConsolePair} and ${secondConsolePair}`)
-      $('#nodeToActivateValue').html('Nope');
-      var nodeToActivate = document.getElementById('nodeToActivateValue');
-      nodeToActivate.style.color = 'gray'; 
-    }
-  }
 }
 
 function clearSecondWheel() {
@@ -208,3 +153,23 @@ function activateValuesInSecondWheel(valuesToActivate) {
   }
 }
 
+function getSecondConsolePairsForFirstConsolePair(secondConsoleData) {
+  console.info(`Getting pairs for second console from first console pair childs: ${JSON.stringifysecondConsoleData}...`)
+
+  const secondConsolePairs = Object.keys(secondConsoleData)
+
+  return secondConsolePairs
+}
+
+function activateValuesInThirdWheel(secondConsolePairs, wheelNumber, positionWhenSpliting) {
+  console.info(`Activating in third wheel values from: ${JSON.stringify(secondConsolePairs)}...`)
+
+  for (let i = 0; i < secondConsolePairs.length; i++) {
+    const pair = secondConsolePairs[i];
+    console.info(pair)
+    const pairValues = pair.split('-')
+    const valueToActivate = pairValues[positionWhenSpliting]
+    $(`#w${wheelNumber}li${valueToActivate}`).removeClass("overlay")
+    console.info(`Activated value in third wheel: ${valueToActivate}.`)
+  }
+}
