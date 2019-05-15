1ba57db7642238d9f816d5fe4f533963b1084797
Joan Comas <joan.comas.fdz@gmail.com>
2019-05-15 10:59:10 +0200

    added void prefix to html elements and put console12 and 13 js files into void subfolder

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-$('#console13-tab').click(function(e) {
+$('#void-console13-tab').click(function(e) {
   $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console12.js"
+    return $(this).attr('src') === "js/void/console12.js"
   }).remove();
-  $.getScript("js/console13.js");
+  $.getScript("js/void/console13.js");
 });
 
-$('#console12-tab').click(function(e) {
+$('#void-console12-tab').click(function(e) {
   $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console13.js"
+    return $(this).attr('src') === "js/void/console13.js"
   }).remove();
-  $.getScript("js/console12.js");
+  $.getScript("js/void/console12.js");
 });
 
 $('#void-tab').click(function(e) {
