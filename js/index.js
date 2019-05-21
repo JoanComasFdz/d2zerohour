49e636e8a77177d6dc0191afae7218a0c140db90
Joan Comas Fernandez <joan.comas.fdz@gmail.com>
2019-05-21 23:56:53 +0200

    added consoles23 to solar

--- js/index.js
+++ js/index.js
@@ -1,76 +1,116 @@
-$(document).ready(function(){
-  $('.nav-tabs').stickyTabs();
-}); 
+$(document).ready(function() {
+  $(".nav-tabs").stickyTabs();
+});
 
 // VOID
-$('#void-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "terminal/voidData.js"
-  }).remove();
+$("#void-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "terminal/voidData.js";
+    })
+    .remove();
   $.getScript("terminal/data/voidData.js");
 });
 
-$('#void-console13-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console12.js"
-  }).remove();
+$("#void-console13-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console12.js";
+    })
+    .remove();
   $.getScript("js/console13.js");
 });
 
-$('#void-console12-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console13.js"
-  }).remove();
+$("#void-console12-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console13.js";
+    })
+    .remove();
   $.getScript("js/console12.js");
 });
 
 // ARC
-$('#arc-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "terminal/arcData.js"
-  }).remove();
+$("#arc-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "terminal/arcData.js";
+    })
+    .remove();
   $.getScript("terminal/data/arcData.js");
 });
 
-$('#arc-console13-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console12.js"
-  }).remove();
+$("#arc-console13-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console12.js";
+    })
+    .remove();
   $.getScript("js/console13.js");
 });
 
-$('#arc-console12-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console13.js"
-  }).remove();
+$("#arc-console12-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console13.js";
+    })
+    .remove();
   $.getScript("js/console12.js");
 });
 
-$('#arc-console23-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console23.js"
-  }).remove();
+$("#arc-console23-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console23.js";
+    })
+    .remove();
   $.getScript("js/console23.js");
 });
 
 // SOLAR
-$('#solar-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "terminal/solarData.js"
-  }).remove();
+$("#solar-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "terminal/solarData.js";
+    })
+    .remove();
   $.getScript("terminal/data/solarData.js");
 });
 
-$('#solar-console13-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console12.js"
-  }).remove();
+$("#solar-console13-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console12.js";
+    })
+    .remove();
   $.getScript("js/console13.js");
 });
 
-$('#solar-console12-tab').click(function(e) {
-  $('html').find('script').filter(function(){
-    return $(this).attr('src') === "js/console13.js"
-  }).remove();
+$("#solar-console12-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console13.js";
+    })
+    .remove();
   $.getScript("js/console12.js");
+});
+
+$("#solar-console23-tab").click(function(e) {
+  $("html")
+    .find("script")
+    .filter(function() {
+      return $(this).attr("src") === "js/console23.js";
+    })
+    .remove();
+  $.getScript("js/console23.js");
 });
